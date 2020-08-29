547457a3c0e7fbe6fa4290374b72955f67455720
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-30 01:33:20 +0700

    build production & etc

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.copyDirectory('resources/fonts', 'public/fonts');
+mix.copyDirectory('resources/images', 'public/images');
+mix.copyDirectory('resources/svg', 'public/svg');
