f6c505a90f2875fc8b875790a5f00f99d27e49cf
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-30 13:59:21 +0700

    add php banner element

---
+++ resources/views/home/components/php-banner.blade.php
@@ -0,0 +1,15 @@
+<div class="d-flex justify-content-between border-bottom">
+    <div>
+        <h2>PHP Hosting</h2>
+        <p>Cepat, handal, penuh dengan modul PHP yang Anda butuhkan</p>
+        <ul>
+            <li>Solusi PHP untuk performa query yang lebih cepat.</li>
+            <li>Konsumsi memori yang lebih rendah.</li>
+            <li>Support PHP 5.3, PHP 5.4, PHP 5.5, PHP 5.6, PHP 7</li>
+            <li>Fitur enkripsi IonCube dan Zend Guard Loaders</li>
+        </ul>
+    </div>
+    <div>
+        <img class="php-banner-illustration" src="{{ asset('svg/illustration banner PHP hosting-01.svg') }}" alt="banner PHP hosting" />
+    </div>
+</div>
