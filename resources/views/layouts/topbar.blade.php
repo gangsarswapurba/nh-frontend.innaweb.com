64166893df93ea6c7fe64e00f31ce6977cd423c4
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-30 13:58:33 +0700

    add top bar element

---
+++ resources/views/layouts/topbar.blade.php
@@ -0,0 +1,10 @@
+<div class="d-flex justify-content-between flex-column flex-md-row p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <div class="">
+        <a href="">icon Gratis Ebook 9 Cara Cerdas Menggunakan Domain[x]</a>
+    </div>
+    <div class="">
+        <a href="">0274-5305505</a>
+        <a href="">Live Chat</a>
+        <a href="">Member Area</a>
+    </div>
+</div>
