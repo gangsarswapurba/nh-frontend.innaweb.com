2ca1828f66395e4f409c60f80896e44521bbbcbe
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-30 10:59:23 +0700

    reorganizing home page layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('home.php-banner')
+
+    @include('home.price')
+
+    @include('home.hosting-features-included')
+
+    @include('home.support-laravel')
+
+    @include('home.php-module')
+
+    @include('home.excellent-customer-support')
+
+@endsection
