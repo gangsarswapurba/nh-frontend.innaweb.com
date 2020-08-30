40aa777481973488bc30fb7e84e5ef9ac1c6c32d
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-30 11:27:11 +0700

    relayout home page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('home.components.php-banner')
+
+    @include('home.components.price')
+
+    @include('home.components.hosting-features-included')
+
+    @include('home.components.support-laravel')
+
+    @include('home.components.php-module')
+
+    @include('home.components.excellent-customer-support')
+
+@endsection
