0bacddebfce67b15123cb586208de5546b8743c0
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-09-01 01:26:55 +0700

    html encryption features

--- resources/views/home/components/encryption-feature.blade.php
+++ resources/views/home/components/encryption-feature.blade.php
@@ -1,20 +1,14 @@
-<div class="d-flex justify-content-center align-items-baseline card-group encryption-feature">
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('images/icon PHP Hosting_zendguard.png') }}" class="card-img-top align-self-center" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('images/icon PHP Hosting_composer.png') }}" class="card-img-top align-self-center" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('images/icon PHP Hosting_ioncube.png') }}" class="card-img-top align-self-center" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
+<div class="d-flex justify-content-between align-items-end encryption-feature text-center m-5">
+        <figure>
+            <img src="{{ asset('images/icon PHP Hosting_zendguard.png') }}" class="align-self-center" alt="...">
+            <p class="mt-5">PHP Zend Guard Loader</p>
+        </figure>
+        <figure>
+            <img src="{{ asset('images/icon PHP Hosting_composer.png') }}" class="align-self-center" alt="...">
+            <p class="mt-5">PHP Composer</p>
+        </figure>
+        <figure>
+            <img src="{{ asset('images/icon PHP Hosting_ioncube.png') }}" class="align-self-center" alt="...">
+            <p class="mt-5">PHP ionCube Loader</p>
+        </figure>
 </div>
