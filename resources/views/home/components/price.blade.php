a41f8466df4f205c6d1178a0db1de6fd61e16b52
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-09-01 01:26:18 +0700

    html card price

--- resources/views/home/components/price.blade.php
+++ resources/views/home/components/price.blade.php
@@ -1,24 +1,29 @@
-<div class="row">
+<div class="row mb-5">
     <div class="col">
         <h2 class="text-center">Paket Hosting Singapura yang Tepat</h2>
         <p class="text-center">Diskon 40% + Domain dan SSL Gratis untuk Anda</p>
     </div>
 </div>
-<div class="row row-cols-1 row-cols-md-4 mb-4 text-center">
+<div class="row row-cols-1 row-cols-md-4 mx-4 text-center">
     <div class="col">
         <div class="card mb-4 shadow-sm">
             <div class="card-header">
                 <h4 class="my-0 font-weight-normal">Bayi</h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+                <h4 class="card-subtitle old-price">Rp 19.900</h4>
+                <h1 class="card-title discount-price">Rp <span>14</span>.900<small class="text-muted">/ mo</small></h1>
+                <p class="card-text">938 Pengguna Terdaftar</p>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Email support</li>
-                    <li>Help center access</li>
+                    <li>0.5X RESOURCE POWER</li>
+                    <li>500 MB Disk Space</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>Unlimited Databases</li>
+                    <li>1 Domain</li>
+                    <li>Instant Backup</li>
+                    <li>Unlimited SSL Gratis Selamanya</li>
                 </ul>
-                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary rounded-pill">Pilih Sekarang</button>
             </div>
         </div>
     </div>
@@ -28,14 +33,21 @@
                 <h4 class="my-0 font-weight-normal">Pelajar</h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+                <h4 class="card-subtitle old-price">Rp 19.900</h4>
+                <h1 class="card-title discount-price">Rp <span>14</span>.900<small class="text-muted">/ mo</small></h1>
+                <p class="card-text">938 Pengguna Terdaftar</p>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>20 users included</li>
-                    <li>10 GB of storage</li>
-                    <li>Priority email support</li>
-                    <li>Help center access</li>
+                    <li>1X RESOURCE POWER</li>
+                    <li>Unlimited Disk Space</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>Unlimited POP3 Email</li>
+                    <li>Unlimited Databases</li>
+                    <li>10 Addon Domains</li>
+                    <li>Instant Backup</li>
+                    <li>Domain Gratis Selamanya</li>
+                    <li>Unlimited SSL Gratis Selamanya</li>
                 </ul>
-                <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary rounded-pill">Pilih Sekarang</button>
             </div>
         </div>
     </div>
@@ -45,14 +57,23 @@
                 <h4 class="my-0 font-weight-normal">Personal</h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+                <h4 class="card-subtitle old-price">Rp 19.900</h4>
+                <h1 class="card-title discount-price">Rp <span>14</span>.900<small class="text-muted">/ mo</small></h1>
+                <p class="card-text">938 Pengguna Terdaftar</p>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>20 users included</li>
-                    <li>10 GB of storage</li>
-                    <li>Priority email support</li>
-                    <li>Help center access</li>
+                    <li>2X RESOURCE POWER</li>
+                    <li>Unlimited Disk Space</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>Unlimited POP3 Email</li>
+                    <li>Unlimited Databases</li>
+                    <li>Unlimited Addon Domains</li>
+                    <li>Instant Backup</li>
+                    <li>Domain Gratis Selamanya</li>
+                    <li>Unlimited SSL Gratis Selamanya</li>
+                    <li>Private Name Server</li>
+                    <li>SpamAssasin Mail Protection</li>
                 </ul>
-                <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary rounded-pill">Pilih Sekarang</button>
             </div>
         </div>
     </div>
@@ -62,14 +83,25 @@
                 <h4 class="my-0 font-weight-normal">Bisnis</h4>
             </div>
             <div class="card-body">
-                <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+                <h4 class="card-subtitle old-price">Rp 19.900</h4>
+                <h1 class="card-title discount-price">Rp <span>14</span>.900<small class="text-muted">/ mo</small></h1>
+                <p class="card-text">938 Pengguna Terdaftar</p>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>30 users included</li>
-                    <li>15 GB of storage</li>
-                    <li>Phone and email support</li>
-                    <li>Help center access</li>
+                    <li>3X RESOURCE POWER</li>
+                    <li>Unlimited Disk Space</li>
+                    <li>Unlimited Bandwidth</li>
+                    <li>Unlimited POP3 Email</li>
+                    <li>Unlimited Databases</li>
+                    <li>Unlimited Addon Domains</li>
+                    <li>Magic Auto Backup & Restore</li>
+                    <li>Domain Gratis Selamanya</li>
+                    <li>Unlimited SSL Gratis Selamanya</li>
+                    <li>Private Name Server</li>
+                    <li>Prioritas Layanan Support</li>
+                    <li>b b b b b</li>
+                    <li>SpamExpert Pro Mail Protection</li>
                 </ul>
-                <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary rounded-pill">Pilih Sekarang</button>
             </div>
         </div>
     </div>
